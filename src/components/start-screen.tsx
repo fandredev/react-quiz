50b1eaa917330ba37d319c47df4bbbbdfa522f99
felipe <profissionalf.andre@gmail.com>
2024-09-21 12:10:50 -0300

    refactor: Update StartScreen component to include dispatch prop and start button functionality

--- src/components/start-screen.tsx
+++ src/components/start-screen.tsx
@@ -1,13 +1,24 @@
+import { Action } from '../hooks/useSearchQuestions';
+
 interface StartScreenProps {
   questionsLength: number;
+  dispatch: React.Dispatch<Action>;
 }
 
-export default function StartScreen({ questionsLength }: StartScreenProps) {
+export default function StartScreen({
+  questionsLength,
+  dispatch,
+}: StartScreenProps) {
   return (
-    <div className="">
+    <div className="start">
       <h2>Welcome to the React Quiz!</h2>
-      <h2>{questionsLength} question to test your React Mastery</h2>
-      <button>Let's start</button>
+      <h3>{questionsLength} question to test your React Mastery</h3>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: 'start' })}
+      >
+        Let's start
+      </button>
     </div>
   );
 }
