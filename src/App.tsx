c1a9035a552fff7fb409160d69dad90cf6f83686
felipe <profissionalf.andre@gmail.com>
2024-09-21 12:10:36 -0300

    refactor: Update useSearchQuestions.ts to include new actions and state properties

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import Loader from './components/loader';
 import ErrorMessage from './components/error';
 import StartScreen from './components/start-screen';
 import { useSearchQuestions } from './hooks/useSearchQuestions';
+import Question from './components/question';
+import NextButton from './components/next-button';
 
 export default function Quiz() {
-  const { questions, status } = useSearchQuestions();
+  const { questions, status, dispatchEvent, index, answer } =
+    useSearchQuestions();
   const questionsLength = questions.length;
 
   return (
@@ -16,7 +19,21 @@ export default function Quiz() {
         {status === 'loading' && <Loader />}
         {status === 'error' && <ErrorMessage />}
         {status === 'ready' && (
-          <StartScreen questionsLength={questionsLength} />
+          <StartScreen
+            questionsLength={questionsLength}
+            dispatch={dispatchEvent}
+          />
+        )}
+
+        {status === 'active' && (
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatchEvent}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatchEvent} answer={answer} />
+          </>
         )}
       </Main>
     </div>
--- src/hooks/useSearchQuestions.ts
+++ src/hooks/useSearchQuestions.ts
@@ -4,16 +4,25 @@ import { Question } from '../shared/interfaces/questions';
 interface State {
   questions: Question[];
   status: 'ready' | 'loading' | 'error' | 'active' | 'finished';
+  index: number
+  answer: null | number
+  points: number
 }
 
 const INITIAL_STATE: State = {
   questions: [],
   status: 'loading',
+  index: 0,
+  answer: null,
+  points: 0
 };
 
-type Action =
+export type Action =
   | { type: 'dataReceived'; payload: Question[] }
-  | { type: 'dataFailed' };
+  | { type: 'dataFailed' }
+  | { type: 'start' }
+  | { type: 'newAnswer'; payload: number }
+  | { type: 'nextQuestion' };
 
 function reducer(state: State, action: Action): State {
   switch (action.type) {
@@ -22,13 +31,35 @@ function reducer(state: State, action: Action): State {
 
     case 'dataFailed':
       return { ...state, status: 'error' };
+    
+    case 'start': {
+      return {
+        ...state,
+        status: 'active',
+      };
+    }
+    case 'newAnswer': {
+      const question = state.questions[state.index];
+      return {
+        ...state,
+        answer: action.payload,
+        points: action.payload === parseInt(question.correctOption) ? state.points + question.points : state.points 
+      }
+    }
+    case 'nextQuestion': {
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null
+      }
+    }
     default:
       throw new Error('Invalid action type');
   }
 }
 
 export function useSearchQuestions() {
-  const [{ status, questions }, dispatch] = useReducer(reducer, INITIAL_STATE);
+  const [{ status, questions, index, answer, points }, dispatch] = useReducer(reducer, INITIAL_STATE);
 
   useEffect(() => {
     async function getQuestions() {
@@ -50,8 +81,13 @@ export function useSearchQuestions() {
     getQuestions();
   }, []);
 
+  
   return {
     questions,
     status,
+    dispatchEvent: dispatch,
+    index,
+    answer,
+    points
   };
 }
